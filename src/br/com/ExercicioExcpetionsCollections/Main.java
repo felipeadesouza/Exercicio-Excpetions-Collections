325fc10f5f370e6f87a4f0b1bb232f9c6da0560e
Felipe <felipeadesouza@gmail.com>
2018-01-18 18:51:57 -0200

    mudança de texto

--- src/br/com/ExercicioExcpetionsCollections/Main.java
+++ src/br/com/ExercicioExcpetionsCollections/Main.java
@@ -3,7 +3,7 @@ package br.com.ExercicioExcpetionsCollections;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("meu projeto");
+		System.out.println("meu projeto novo");
 	}
 
 }
